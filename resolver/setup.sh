8d8a60e46d6f41bca6f87a46656a5de77446c6f9
Joachim Hill-Grannec <joachim.hill@gmail.com>
2020-02-25 12:51:05 -0600

    mkdir -p the /etc/resolver directory if it doesn't exist create it remove the resolver domains when tearing down

--- resolver/setup.sh
+++ resolver/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo chown root ./networkchange.plist
 cp setresolver.sh /Users/Shared/setresolver.sh
+sudo mkdir -p /etc/resolver
 sudo launchctl load networkchange.plist
--- resolver/teardown.sh
+++ resolver/teardown.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo rm /Users/Shared/setresolver.sh
+sudo rm /etc/resolver/pelo.tech
+sudo rm /etc/resolver/amazonaws.com
 sudo launchctl unload networkchange.plist
